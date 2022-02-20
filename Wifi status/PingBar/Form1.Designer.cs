c5bf8d2fc3cd892a90b10a8b5952f79b5b5def7a
192.168.0.1 <90830312+negativexp@users.noreply.github.com>
2022-02-20 22:31:45 +0100

    Add files via upload

---
+++ Wifi status/PingBar/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace PingBar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelWifiStatus = new System.Windows.Forms.Label();
+            this.timerCheckWifiStatus = new System.Windows.Forms.Timer(this.components);
+            this.labelPingStatus = new System.Windows.Forms.Label();
+            this.timerStopper = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.timerIsFocused = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelWifiStatus
+            // 
+            this.labelWifiStatus.AutoSize = true;
+            this.labelWifiStatus.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.labelWifiStatus.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelWifiStatus.ForeColor = System.Drawing.Color.Red;
+            this.labelWifiStatus.Location = new System.Drawing.Point(6, 16);
+            this.labelWifiStatus.Name = "labelWifiStatus";
+            this.labelWifiStatus.Size = new System.Drawing.Size(88, 14);
+            this.labelWifiStatus.TabIndex = 0;
+            this.labelWifiStatus.Text = "DISCONNECTED";
+            // 
+            // timerCheckWifiStatus
+            // 
+            this.timerCheckWifiStatus.Interval = 1000;
+            this.timerCheckWifiStatus.Tick += new System.EventHandler(this.timerCheckWifiStatus_Tick);
+            // 
+            // labelPingStatus
+            // 
+            this.labelPingStatus.AutoSize = true;
+            this.labelPingStatus.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.labelPingStatus.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelPingStatus.ForeColor = System.Drawing.Color.White;
+            this.labelPingStatus.Location = new System.Drawing.Point(6, 34);
+            this.labelPingStatus.Name = "labelPingStatus";
+            this.labelPingStatus.Size = new System.Drawing.Size(46, 14);
+            this.labelPingStatus.TabIndex = 1;
+            this.labelPingStatus.Text = "Ping: ...";
+            // 
+            // timerStopper
+            // 
+            this.timerStopper.Interval = 1000;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.labelWifiStatus);
+            this.groupBox1.Controls.Add(this.labelPingStatus);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.groupBox1.ForeColor = System.Drawing.Color.Black;
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(100, 58);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // timerIsFocused
+            // 
+            this.timerIsFocused.Interval = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(134, 90);
+            this.Controls.Add(this.groupBox1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelWifiStatus;
+        private System.Windows.Forms.Timer timerCheckWifiStatus;
+        private System.Windows.Forms.Label labelPingStatus;
+        private System.Windows.Forms.Timer timerStopper;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Timer timerIsFocused;
+    }
+}
+
---
+++ Wifi status/PingBar/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows;
+
+using System.Net;
+using System.Timers;
+using System.Net.NetworkInformation;
+
+namespace PingBar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.BackColor = Color.Magenta;
+            this.TransparencyKey = Color.Magenta;
+
+            Rectangle res = Screen.PrimaryScreen.Bounds;
+            this.Location = new Point(res.Width - Size.Width, res.Height - Size.Height - 25);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timerCheckWifiStatus.Start();
+            timerStopper.Start();
+            timerIsFocused.Start();
+        }
+
+        static bool CheckIfConnected()
+        {
+            bool connection = NetworkInterface.GetIsNetworkAvailable();
+            if(connection) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        static int GetPing()
+        {
+            Ping ping = new Ping();
+            PingReply reply = ping.Send("216.58.201.78", 1000);
+            if (reply != null) {
+                return Convert.ToInt32(reply.RoundtripTime);
+            }
+            else {
+                return Convert.ToInt32(reply.RoundtripTime);
+            }
+
+
+        }
+
+        private void timerCheckWifiStatus_Tick(object sender, EventArgs e)
+        {
+            if (CheckIfConnected()) {
+                labelWifiStatus.Text = "CONNECTED";
+                labelWifiStatus.ForeColor = Color.Green;
+                labelPingStatus.Text = "Ping: " + GetPing().ToString();
+            } else if (!CheckIfConnected()) {
+                labelWifiStatus.Text = "DISCONNECTED";
+                labelWifiStatus.ForeColor = Color.Red;
+            }
+        }
+    }
+}
