7a44d663f1c12f24a12e1a485d8f89ec1dbfe580
192.168.0.1 <90830312+negativexp@users.noreply.github.com>
2022-02-22 01:52:58 +0100

    Add files via upload

---
+++ class/Classes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentClass student = new StudentClass();
+            student.Name = "matyas";
+            student.Age = 16;
+            student.Phone = "+420 775 626 290";
+            Console.WriteLine(student.Name);
+            Console.WriteLine(student.Age);
+            Console.WriteLine(student.Phone);
+        }
+    }
+}
