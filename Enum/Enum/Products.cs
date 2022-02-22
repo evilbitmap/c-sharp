91deb57d26f333fa269b1b819f75b6311b9b6ed5
192.168.0.1 <90830312+negativexp@users.noreply.github.com>
2022-02-22 02:09:59 +0100

    Add files via upload

---
+++ Enum/Enum/Products.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Enum
+{
+    class Products
+    {
+        public enum ProductCodes
+        {
+            mleko = 0,
+            jablko = 1,
+            hruska = 2,
+            caj = 3,
+            NIC = 0x0
+        }
+    }
+}
---
+++ Enum/Enum/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Products.ProductCodes.hruska); // output: hruska
+
+            Console.WriteLine((int)Products.ProductCodes.hruska); // output: 2 
+            Console.WriteLine((int)Products.ProductCodes.mleko); // output: 0
+            Console.WriteLine((int)Products.ProductCodes.NIC); // output: 0
+
+            Console.WriteLine((Products.ProductCodes)3); // output: caj
+        }
+    }
+}
