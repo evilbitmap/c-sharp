b1c3de83880beb36835039ee0ed3b7f26ffa0c5b
192.168.0.1 <90830312+negativexp@users.noreply.github.com>
2022-02-22 17:13:12 +0100

    Add files via upload

---
+++ Vlastnosti/Vlastnosti/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Vlastnosti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            data data = new data();
+            data._x = input;
+
+            Console.WriteLine("");
+            Console.WriteLine(data._x);
+            Console.WriteLine(data._y);
+            data._string = data._string + "123";
+            Console.WriteLine(data._string);
+
+        }
+    }
+}
---
+++ Vlastnosti/Vlastnosti/data.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vlastnosti
+{
+    class data
+    {
+        public int _x { get; set; }
+        public int _y { get; set; }
+        public string _string { get; set; } = "bruh";
+    }
+}
