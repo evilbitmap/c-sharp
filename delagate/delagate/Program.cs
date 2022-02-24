13089717b7f02a99b90d63b0e439c45445a41669
192.168.0.1 <90830312+negativexp@users.noreply.github.com>
2022-02-24 23:00:53 +0100

    Add files via upload

--- delagate/delagate/Program.cs
+++ delagate/delagate/Program.cs
@@ -4,27 +4,46 @@ namespace delagate
 {
     class Program
     {
+        //delegate
+        public delegate void CallbackDelegateString(string msg);
+
         static void Main(string[] args)
         {
+            //objekt 
             WorkClass worker1 = new WorkClass();
-            worker1.DoWork(Callback);
+            //vyvolání objektu s Callback funkcí
+            worker1.DoWork(CallbackFunciton);
+
+            //přiřadíme k "CallbackDelegateString" jméno a funkci
+            CallbackDelegateString callbackstring = DelegateStringFunction;
+            //vyvoláme callback
+            callbackstring("mrdko");
         }
-        static void Callback(int i)
+
+        //vypíše i 
+        static void CallbackFunciton(int i)
         {
             Console.WriteLine(i);
         }
-
+        //vypíše message
+        static void DelegateStringFunction(string message)
+        {
+            Console.WriteLine(message);
+        }
     }
     class WorkClass
     {
+        //delegate
         public delegate void CallbackDelegate(int x);
-        public void DoWork(CallbackDelegate callbackdelegate)
+
+        public void DoWork(CallbackDelegate callback)
         {
-            for (int i = 0; i < 999999; i++)
+            // for loop (nějaká práce)
+            for (int i = 0; i <= 9999; i++)
             {
-                callbackdelegate(i);
+                //
+                callback(i);
             }
         }
-
     }
 }
