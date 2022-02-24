b1bbfae86e3947bce62a8eede7b29a0c59886175
192.168.0.1 <90830312+negativexp@users.noreply.github.com>
2022-02-24 22:49:20 +0100

    Add files via upload

---
+++ delagate/delagate/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace delagate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WorkClass worker1 = new WorkClass();
+            worker1.DoWork(Callback);
+        }
+        static void Callback(int i)
+        {
+            Console.WriteLine(i);
+        }
+
+    }
+    class WorkClass
+    {
+        public delegate void CallbackDelegate(int x);
+        public void DoWork(CallbackDelegate callbackdelegate)
+        {
+            for (int i = 0; i < 999999; i++)
+            {
+                callbackdelegate(i);
+            }
+        }
+
+    }
+}
